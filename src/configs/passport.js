e570a43c275ab7eb9012498e142f33daf673fdef
Alex <cheuk.alex@gmail.com>
2020-03-02 10:00:38 -0800

    chore: add GITHUB_CALLBACK_URL

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -4,7 +4,7 @@ import passport from 'passport'
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: 'https://freezemerge.ngrok.io/auth/github/callback'
+  callbackURL: process.env.GITHUB_CALLBACK_URL
 },
 function (accessToken, refreshToken, profile, cb) {
   // console.log(accessToken, refreshToken, profile)
